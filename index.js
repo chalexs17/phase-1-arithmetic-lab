131e054588f0910160b9699bdf384efc43a2ecd6
Alexis Chavez <alexis.chavez17@outlook.com>
2023-03-19 09:07:13 -0700

    Complete Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
+
 // Write your code here
+//function multiply() {}
+    let num1 = 31;
+    let num2 = 2;
+    const multiply = 31 * 2;
+
+//function random() {}
+    let random = Math.floor(Math.random() * 100) + 1;
+
+
+//function mod() {}
+    let num3 = 22;
+    let num4 = 18
+    const mod = num3 % num4;
+
+//function max(numbers) {}
+    let array = [1, 2, 3, 4, 5, 20]
+    let max = Math.max(...array);
+//return Math.max(...numbers);
